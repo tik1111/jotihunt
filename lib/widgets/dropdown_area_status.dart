7a784dd73bc59473653a988e01adedf05d05b288
tik111 <tim-van-der-maas@hotmail.com>
2023-08-31 23:12:53 +0200

    Updates in area

--- lib/widgets/dropdown_area_status.dart
+++ lib/widgets/dropdown_area_status.dart
@@ -3,17 +3,6 @@ import 'package:jotihunt/cubitAndStream/stream_provider.dart';
 import 'package:jotihunt/handlers/handler_area_status.dart';
 import 'package:jotihunt/handlers/handler_secure_storage.dart';
 
-/// Flutter code sample for [DropdownMenu].
-
-const List<String> list = <String>[
-  'Alpha',
-  'Bravo',
-  'Charlie',
-  'Delta',
-  'Echo',
-  'Foxtrot'
-];
-
 class DropdownMenuApp extends StatelessWidget {
   const DropdownMenuApp({super.key});
 
@@ -31,61 +20,48 @@ class DropdownMenuAreaStatus extends StatefulWidget {
 }
 
 class _DropdownMenuAreaStatusState extends State<DropdownMenuAreaStatus> {
-  String dropdownValue = list.first;
   List<DropdownMenuEntry<String>> dropdownitems = [];
-  String initialarea = "Bravo";
+
+  String initialarea = "";
   Color currentIconColor = Colors.red;
 
   Future<List<DropdownMenuEntry<String>>> loadAreaStatus() async {
-    await SecureStorage().writeCurrentArea(initialarea);
+    Future<List<DropdownMenuEntry<String>>> allAreaStatus =
+        AreaStatusHandler().getAllAreaStatusDropDownMenuEntry();
+    return allAreaStatus;
+  }
 
-    return AreaStatusHandler().getAllAreaStatus();
+  Future<String> getLastKnownSelectedArea() async {
+    String? getCurrentSelectedArea =
+        await SecureStorage().getCurrentSelectedArea();
+
+    if (getCurrentSelectedArea != "" || getCurrentSelectedArea != null) {
+      initialarea = getCurrentSelectedArea!;
+    }
+    return getCurrentSelectedArea.toString();
   }
 
   @override
   void initState() {
     super.initState();
     loadAreaStatus().then((value) {
-      dropdownitems = [];
-
       setState(() {
-        switch (value.first.value) {
-          case 'red':
-            currentIconColor = Colors.red;
-            break;
-          case 'orange':
-            currentIconColor = Colors.orange;
-            break;
-          case 'green':
-            currentIconColor = Colors.green;
-            break;
-          default:
-            currentIconColor = Colors.red;
-            break;
-        }
-
         dropdownitems = value;
       });
     });
+
+    getLastKnownSelectedArea().then((value) {
+      setState(() {
+        initialarea = value;
+      });
+    });
+
     areaStatusUpdateStream.getResponse.listen((event) {
       if (mounted) {
         setState(() {
           loadAreaStatus().then((value) {
             setState(() {
-              switch (value.first.value) {
-                case 'red':
-                  currentIconColor = Colors.red;
-                  break;
-                case 'orange':
-                  currentIconColor = Colors.orange;
-                  break;
-                case 'green':
-                  currentIconColor = Colors.green;
-                  break;
-                default:
-                  currentIconColor = Colors.red;
-                  break;
-              }
+              //! do the same as onselected
               dropdownitems = value;
             });
           });
@@ -101,32 +77,33 @@ class _DropdownMenuAreaStatusState extends State<DropdownMenuAreaStatus> {
       child: DropdownMenu<String>(
         menuStyle: const MenuStyle(
             backgroundColor: MaterialStatePropertyAll(Colors.white)),
-        initialSelection:
-            dropdownitems.isNotEmpty ? dropdownitems.first.value : null,
+        initialSelection: dropdownitems.isNotEmpty ? initialarea : null,
         trailingIcon: Icon(
           Icons.circle,
           color: currentIconColor,
         ),
         onSelected: (String? value) async {
-          // This is called when the user selects an item.
           await SecureStorage().writeCurrentArea(value.toString());
-          setState(() {
-            switch (value) {
-              case 'red':
-                currentIconColor = Colors.red;
-                break;
-              case 'orange':
-                currentIconColor = Colors.orange;
-                break;
-              case 'green':
-                currentIconColor = Colors.green;
-                break;
-              default:
-                currentIconColor = Colors.red;
-                break;
+          var AreaStatus = await AreaStatusHandler().getAllAreaStatusString();
+          AreaStatus.forEach((element) {
+            if (element['name'] == value.toString()) {
+              setState(() {
+                switch (element['status']) {
+                  case 'red':
+                    currentIconColor = Colors.red;
+                    break;
+                  case 'orange':
+                    currentIconColor = Colors.orange;
+                    break;
+                  case 'green':
+                    currentIconColor = Colors.green;
+                    break;
+                  default:
+                    currentIconColor = Colors.red;
+                    break;
+                }
+              });
             }
-
-            dropdownValue = value!;
           });
         },
         dropdownMenuEntries: dropdownitems,
