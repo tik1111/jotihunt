cd3df6af955855200a605519b0e8a55764f41a2d
tik111 <tim-van-der-maas@hotmail.com>
2023-09-07 23:07:29 +0200

    Feat: Huntcode field in alert dialog

--- lib/widgets/alertdiolog_hunt_or_spot.dart
+++ lib/widgets/alertdiolog_hunt_or_spot.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:jotihunt/handlers/handler_locations.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:jotihunt/handlers/handler_locations.dart';
 
 class HuntOrSpotAlertDialog extends StatefulWidget {
   final LatLng point;
@@ -12,78 +11,66 @@ class HuntOrSpotAlertDialog extends StatefulWidget {
       : _formKeyHuntOrSpot = formKey;
 
   @override
-  // ignore: library_private_types_in_public_api
   _HuntOrSpotAlertDialogState createState() => _HuntOrSpotAlertDialogState();
 }
 
 class _HuntOrSpotAlertDialogState extends State<HuntOrSpotAlertDialog> {
   bool showTextField = false;
   TextEditingController textController = TextEditingController();
+  String? validationMessage;
+
+  void validateAndSubmit() {
+    if (textController.text.isEmpty) {
+      setState(() {
+        validationMessage = 'Vul de hunt code in';
+      });
+    } else {
+      Navigator.of(context).pop();
+      LocationHandler().addHuntOrSpot(widget.point, "hunt");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      content: Stack(
-        clipBehavior: Clip.none,
-        children: <Widget>[
-          Form(
-            key: widget._formKeyHuntOrSpot,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    child: const Text("Hunt"),
-                    onPressed: () {
-                      if (widget._formKeyHuntOrSpot.currentState!.validate()) {
-                        Navigator.of(context).pop();
-                        LocationHandler().addHuntOrSpot(widget.point, "hunt");
-                      }
-                    },
-                  ),
-                ),
-                if (showTextField)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: textController,
-                      decoration:
-                          const InputDecoration(labelText: 'Voer huntcode in'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a hunt code';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    child: const Text("Spot"),
-                    onPressed: () {
-                      LocationHandler().addHuntOrSpot(widget.point, "spot");
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ),
-                if (showTextField)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      child: const Text("Submit"),
-                      onPressed: () {
-                        // Voer actie uit met de tekst uit het tekstveld
-                        Navigator.of(context).pop();
-                        LocationHandler().addHuntOrSpot(widget.point, "hunt");
-                      },
-                    ),
-                  ),
-              ],
+      content: Form(
+        key: widget._formKeyHuntOrSpot,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            CupertinoButton(
+              child: const Text("Spot"),
+              onPressed: () {
+                LocationHandler().addHuntOrSpot(widget.point, "spot");
+                Navigator.of(context).pop();
+              },
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              height: 1,
+              color: CupertinoColors.systemGrey,
+            ),
+            Container(
+              height: 20,
+            ),
+            CupertinoTextField(
+              controller: textController,
+              placeholder: 'Voer huntcode in',
+            ),
+            if (validationMessage != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  validationMessage!,
+                  style: const TextStyle(color: CupertinoColors.destructiveRed),
+                ),
+              ),
+            CupertinoButton(
+              onPressed: validateAndSubmit,
+              child: const Text("Hunt"),
+            ),
+          ],
+        ),
       ),
     );
   }
